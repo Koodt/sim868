64ddf2c92ac54fb454db8ee1a4fca86ce937edba
HEBCKUU_KOT <kod-t@mail.ru>
2018-10-10 21:56:00 +0300

    fix listen

--- listen.py
+++ listen.py
@@ -4,16 +4,17 @@ import socket
 
 sock = socket.socket()
 
-sock.bind(('', 5003))
-sock.listen(1)
+sock.bind( ('', 5003) )
+sock.listen(5)
 conn, addr = sock.accept()
+conn.settimeout(10)
+data = conn.recv(1024)
 
-print 'connected: ', addr
+if not data:
+    print('No data')
+    conn.close()
 
-while True:
-        data = conn.recv(1024)
-        if not data:
-                break
-        conn.send(data.upper())
+udata = data.decode('utf-8')
+print('Data: ' + udata)
 
 conn.close()
