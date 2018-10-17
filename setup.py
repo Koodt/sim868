b71ab2e81975efefad06b447ead192ae9ab02b91
HEBCKUU_KOT <kod-t@mail.ru>
2018-10-17 21:18:22 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from pip.req import parse_requirements
+from setuptools import setup
+import uuid
+
+setup(
+        name='ksim868',
+        version='0.0.1',
+        modules=['ksim868'],
+        scripts=['ksim868.py'],
+        url='https://github.com/Koodt/sim868',
+        license='MIT License',
+        author='Koodt',
+        author_email='k0dt@k0dt.ru',
+        description='SIM868 based TCP server and client.',
+        install_requires= [str(ir.req) for ir in parse_requirements('requirements.txt', session=uuid.uuid1())]
+)
