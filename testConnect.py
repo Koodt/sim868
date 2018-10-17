0a8de7495d623844e536af3b462a046e35628d4e
HEBCKUU_KOT <kod-t@mail.ru>
2018-10-17 23:30:28 +0300

    Update testConnect.py

--- testConnect.py
+++ testConnect.py
@@ -5,54 +5,60 @@ import sys
 from Crypto.Cipher import AES
 from Crypto import Random
 
-key = b'f861feab561441c0e1fdcba91581dd95'
+
+def getDataFromJSON():
+    with open("default.json") as sourceFile:
+        data = json.load(sourceFile)
+        return (
+            data["targetHost"],
+            int(data["targetPort"]),
+            bytes(data["messages"][0]),
+            bytes(data["AESkey"]),
+        )
+
+
+targetHost, targetPort, message, key = getDataFromJSON()
 iv = Random.new().read(AES.block_size)
 obj = AES.new(key, AES.MODE_CBC, iv)
 
-def getDataFromJSON():
-    with open('default.json') as sourceFile:
-        data = json.load(sourceFile)
-        return data['targetHost'], int(data['targetPort']), data['messages'][1]
 
 def encryptData(message):
     encryptText = iv + obj.encrypt(message)
     return encryptText
 
+
 def decryptData(message):
     decryptText = obj.decrypt(message)
     return decryptText
 
+
 def get_constants(prefix):
-    return dict( (getattr(socket, n), n)
-                 for n in dir(socket)
-                 if n.startswith(prefix)
-                 )
+    return dict((getattr(socket, n), n) for n in dir(socket) if n.startswith(prefix))
 
-targetHost, targetPort, message = getDataFromJSON()
 
-families = get_constants('AF_')
-types = get_constants('SOCK_')
-protocols = get_constants('IPPROTO_')
+families = get_constants("AF_")
+types = get_constants("SOCK_")
+protocols = get_constants("IPPROTO_")
 
 # Create a TCP/IP socket
-sock = socket.create_connection((targetHost, targetPort))
+try:
+    sock = socket.create_connection((targetHost, targetPort))
+except socket.error as errMessage:
+    print >>sys.stderr, "Socket error %s" % errMessage
+    sys.exit(0)
 
-print >> sys.stderr, 'Family  :', families[sock.family]
-print >> sys.stderr, 'Type    :', types[sock.type]
-print >> sys.stderr, 'Protocol:', protocols[sock.proto]
-print >> sys.stderr
+print >>sys.stderr, "Family   :", families[sock.family]
+print >>sys.stderr, "Type     :", types[sock.type]
+print >>sys.stderr, "Protocol :", protocols[sock.proto]
+print >>sys.stderr
 
 try:
-
-    # Send data
-    #message = raw_input()
-#    message = b'1111111111111111'
     encryptMessage = encryptData(message)
-    print >> sys.stderr, 'sending "%s"' % encryptMessage
+    print >>sys.stderr, 'sending "%s"' % encryptMessage
     sock.sendall(encryptMessage)
     decryptMessage = decryptData(encryptMessage)
-    print >> sys.stderr, 'local decrypting "%s"' % decryptMessage
+    print >>sys.stderr, 'local decrypting "%s"' % decryptMessage
 
 finally:
-    print >> sys.stderr, 'closing socket'
+    print >>sys.stderr, "closing socket"
     sock.close()
