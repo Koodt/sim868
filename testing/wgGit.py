ca6905fff92ab5c3964601c8f11aad7e889dc71f
HEBCKUU_KOT <kod-t@mail.ru>
2018-11-03 13:18:26 +0300

    Add gitClass

---
+++ testing/wgGit.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+import git
+
+class GitClass(object):
+    def __init__(self, url, path):
+        self.url = url
+        self.path = path
+        self._repo = None
+
+    def dlRepo(self):
+        self._repo = git.Repo.clone_from(self.url, self.path)
+
+    @property
+    def repo(self):
+        if self._repo is None:
+            self.fetch_repo()
+        return self._repo
+
+
+if __name__ == "__main__":
+    GitClass.dlRepo("https://github.com/Koodt/sim868.git", "D:\kill")
