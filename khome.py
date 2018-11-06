fa13d32a7d67eeac21a97c1841e50f697e73dcb8
HEBCKUU_KOT <kod-t@mail.ru>
2018-11-06 17:38:54 +0300

    import keyGen

--- khome.py
+++ khome.py
@@ -6,6 +6,7 @@ import json
 import argparse
 from Crypto.Cipher import AES
 from Crypto import Random
+from khomeGen import createKeysPair
 
 def getDataFromJSON(filename):
     try:
@@ -33,7 +34,11 @@ def bindSocket():
 parser = argparse.ArgumentParser(description='khome')
 parser.add_argument('-r', '--role', action='store', dest='khomeRole', help='select role from collector and harvester')
 parser.add_argument('-f', '--file', action='store', dest='filename', nargs='?', type=argparse.FileType('r'), help='Set full path to parsing file')
+parser.add_argument('-k', '--key', action='store_true', help='create keys pair')
 results = parser.parse_args()
+
+if results.key:
+    createKeysPair()
 
 if results.khomeRole == 'collector':
     print('collector')
@@ -41,6 +46,7 @@ elif results.khomeRole == 'harvester':
     print('harvester')
 else:
     print('What do you want, Jackson?')
+    sys.exit()
 
 data = getDataFromJSON(results.filename)
 
