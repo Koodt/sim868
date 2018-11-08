c5311cce7900623bf8e0b4f079f943021832fd76
HEBCKUU_KOT <kod-t@mail.ru>
2018-11-08 14:28:50 +0300

    extrack functions to single file

--- khome.py
+++ khome.py
@@ -6,30 +6,7 @@ import json
 import argparse
 from Crypto.Cipher import AES
 from Crypto import Random
-from kLibs import Kdefault
-
-def getDataFromJSON(filename):
-    try:
-        with filename as sourceFile:
-            data = json.load(sourceFile)
-            return data
-    except IOError as errMessage:
-        print >>sys.stderr, "I/O error(%s): %s - %s" % (
-            errMessage.errno,
-            errMessage.filename,
-            errMessage.strerror,
-        )
-        sys.exit(0)
-
-def bindSocket():
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    serverAddress = ('', int(getDataFromJSON()["targetPort"]))
-    print 'starting up on %s port %s' % serverAddress
-    try:
-        sock.bind(serverAddress)
-    except socket.error as message:
-        print >> sys.stderr, 'Socket error %s' % message
-        sys.exit(0)
+from kLibs import Kdefault, Binder, Kjson
 
 parser = argparse.ArgumentParser(description='khome')
 parser.add_argument('-r', '--role', action='store', dest='khomeRole', help='select role from collector and harvester')
@@ -57,7 +34,7 @@ else:
     print('What do you want, Jackson?')
     sys.exit()
 
-data = getDataFromJSON(results.filename)
+data = Kjson(results.filename)
 
 if bool(data["services"]["RSAgenerator"]["subscribe"]) == True:
     print("True")
