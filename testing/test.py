1132f08a7759aba031ef8684c2bf99f3b7b41dc4
HEBCKUU_KOT <kod-t@mail.ru>
2018-11-19 23:09:54 +0300

    Update test.py

--- testing/test.py
+++ testing/test.py
@@ -1,1 +1,7 @@
 import sqlite3
+
+dbConnect = sqlite3.connect('/srv/testing.db')
+dbCursor = dbConnect.cursor()
+dbCursor.execute('CREATE TABLE testTable (Count INTEGER, Item TEXT, Quantity INTEGER)')
+dbConnect.commit()
+dbConnect.close()
