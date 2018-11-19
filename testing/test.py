e7f6c577cbe7e8b0c55297d008c85f5e62688d63
HEBCKUU_KOT <kod-t@mail.ru>
2018-11-19 23:22:57 +0300

    Update test.py

--- testing/test.py
+++ testing/test.py
@@ -1,7 +1,10 @@
+#!/usr/bin/python3
+
 import sqlite3
 
 dbConnect = sqlite3.connect('/srv/testing.db')
 dbCursor = dbConnect.cursor()
-dbCursor.execute('CREATE TABLE testTable (Count INTEGER, Item TEXT, Quantity INTEGER)')
+dbCursor.execute('CREATE TABLE IF NOT EXISTS storeTable (ID int NOT NULL AUTO_INCREMENT, Item varchar(255) NOT NULL, Quantity int NOT NULL, PRIMARY KEY (ID))')
+#dbCursor.execute('INSERT INTO testTable VALUES ()')
 dbConnect.commit()
 dbConnect.close()
