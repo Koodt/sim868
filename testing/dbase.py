25c27e6a7759cd20fe1c39974162c3c731813972
HEBCKUU_KOT <kod-t@mail.ru>
2019-05-11 00:41:01 +0300

    add testing content

--- testing/dbase.py
+++ testing/dbase.py
@@ -1,3 +1,15 @@
 from pymongo import MongoClient
 
 client = MongoClient('mongodb://localhost:27017')
+dbase  = client.pymongo_test
+posts  = dbase.posts
+
+post_data = {
+    'title': 'mongo',
+    'content': 'mongo again',
+    'author': 'no mongo'
+}
+
+result = posts.insert_one(post_data)
+
+print('Post: %s' % result.inserted_id)
