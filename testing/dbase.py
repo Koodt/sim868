3c43f8d4736a8a8c657f4c443f5771214794c4c5
HEBCKUU_KOT <kod-t@mail.ru>
2019-05-14 23:29:49 +0300

    create pymongo class

--- testing/dbase.py
+++ testing/dbase.py
@@ -1,15 +1,41 @@
-from pymongo import MongoClient
+import pymongo
 
-client = MongoClient('mongodb://localhost:27017')
-dbase  = client.pymongo_test
-posts  = dbase.posts
+class MongoStatic:
+    def __init__(self, inHost):
+        self.dHost = inHost
+        self.client = pymongo.MongoClient(self.dHost)
 
-post_data = {
-    'title': 'mongo',
-    'content': 'mongo again',
-    'author': 'no mongo'
-}
+    def dBaseList(self):
+        return self.client.list_database_names()
 
-result = posts.insert_one(post_data)
+    def baseCheck(self, inBase):
+        self.dBase = inBase
+        if inBase in self.dBaseList():
+            return False
+        else:
+            return True
 
-print('Post: %s' % result.inserted_id)
+    def baseCreate(self, inBase):
+        self.inBase = inBase
+        dBase = self.client.self.inBase
+
+    def collectionCreate():
+        self.posts = self.dBase.posts
+
+    def toCollection():
+        post_data = {
+            'title': 'mongo',
+            'content': 'mongo again',
+            'author': 'no mongo'
+        }
+        result = posts.insert_one(post_data)
+
+    def collectionFindAll():
+        return
+
+testInstance = MongoStatic('mongodb://localhost:27017')
+print(testInstance.dBaseList())
+if testInstance.baseCheck('pymongo_test'):
+    testInstance.baseCreate('pymongo_est')
+else:
+    print('Nope')
