51421f24a9377b9d7e3dd703fb46aa2738c790f0
HEBCKUU_KOT <kod-t@mail.ru>
2019-05-15 15:17:41 +0300

    pymongo +1

--- kMongo.py
+++ kMongo.py
@@ -8,17 +8,8 @@ class MongoStatic:
     def dBaseList(self):
         return self.client.list_database_names()
 
-    def collList(self):
-        return self.client.self.dBase.list_collection_names()
-
     def baseCheck(self):
         if self.dBase in self.dBaseList():
-            return False
-        else:
-            return True
-
-    def collCheck(self):
-        if self.inColl in self.baseCheck():
             return False
         else:
             return True
@@ -27,7 +18,7 @@ class MongoStatic:
         self.myDb = self.client[self.dBase]
 
     def collectionCreate(self, inColl):
-        self.posts = self.dBase[inColl]
+        self.posts = self.myDb[inColl]
 
     def toCollection(self, postData):
         self.postData = postData
@@ -35,14 +26,3 @@ class MongoStatic:
 
     def collectionFindAll():
         return
-
-testInstance = MongoStatic('pymongo_guest', 'mongodb://localhost:27017')
-print(testInstance.dBaseList())
-if testInstance.baseCheck():
-    testInstance.baseCreate()
-else:
-    print('Dbase exist')
-
-if testInstance.colCheck():
-    testInstance.collectionCreate('test')
-    testInstance.toCollection({'title': 'mongo', 'content': 'mongo again', 'author': 'no mongo'})
